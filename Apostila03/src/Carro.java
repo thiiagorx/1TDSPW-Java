c002bd0f2f969137eabfd9cc8d543220473b67aa
thiiagorx <thiiagorx@gmail.com>
2026-03-11 22:50:59 -0300

    Projeto da Apostila 03

---
+++ Apostila03/src/Carro.java
@@ -0,0 +1,21 @@
+public class Carro {
+    String modelo;
+    int velocidade;
+    String cor;
+    double peso;
+    short ano;
+    float altura;
+    boolean automatico;
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Apostila03/src/Teste.java
@@ -0,0 +1,27 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+            System.out.println("Boa noite");
+
+
+            //Declarar variaveis
+            boolean ativo;
+            //Atribuir um valor para a variavel
+            ativo = true; //false
+            //Declarar variaveis e atribuir o valor
+            char sexo = 'F';
+            byte idade = 19;
+            short rm = 555;
+            int ano = 2026;
+            long id = 151;
+            float altura = 1.80f; //f no final quer dizer float
+            double preco = 150.25;
+            String nome = "Ricardo";
+
+
+
+
+
+            }
+}
