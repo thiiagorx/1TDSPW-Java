3f017bb7322ff48c810a902134e9608eb1bd0826
thiiagorx <thiiagorx@gmail.com>
2026-03-13 22:56:39 -0300

    Classes e Objetos

---
+++ Apostila03/src/ExemploCarro.java
@@ -0,0 +1,64 @@
+public class ExemploCarro {
+
+
+    public static void main(String[] args) {
+        //Criar um objeto do tipo carro
+        Carro gol = new Carro();
+        gol.cor = "Pratra";
+        gol.velocidade = 120;
+        gol.ano = 2003;
+        gol.altura = 1.5f; //f no final para definir que o valor é float
+        gol.peso = 500;
+        gol.automatico = false; //true (verdadeiro), false (falso)
+        gol.modelo = "Gol Special Bola";
+
+        //Exibir os valores dos atributos do objeto
+        System.out.println("Modelo: " + gol.modelo); //sout
+        System.out.println("Cor: " +  gol.cor);
+        System.out.println("ANO: " + gol.ano);
+        System.out.println("Peso " + gol.peso);
+        System.out.println("Modelo " + gol.modelo);
+        System.out.println("Altura " + gol.altura);
+        System.out.println("Velociade " + gol.velocidade);
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
